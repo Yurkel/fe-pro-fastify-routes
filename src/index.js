8c645c6437a554e5328fc4b2476a71d53383073f
Yuriy <yuriy-k@ukr.net>
2022-11-03 21:25:03 +0300

    all done

--- src/index.js
+++ src/index.js
@@ -11,4 +11,76 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+const verify = function (endpoint, requestBody, reply) {
+  let result;
+
+  if (requestBody.toLowerCase().includes('fuck')) {
+    return reply.status(403).send('unresolved');
+  }
+
+  if (endpoint === '/uppercase') {
+    result = requestBody.toUpperCase();
+  } else if (endpoint === '/lowercase') {
+    result = requestBody.toLowerCase();
+  }
+
+  return reply.send(result);
+};
+
+fastify.post('/uppercase', (request, reply) => {
+  return verify('/uppercase', request.body, reply);
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  return verify('/lowercase', request.body, reply);
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  const { id } = request.params;
+  if (id in users) {
+    return reply.send(users[id]);
+  } else {
+    return reply.status(400).send('User not exist');
+  }
+});
+
+fastify.get('/users', (request, reply) => {
+  const { filter, value } = request.query;
+
+  if (!filter || !value) {
+    return reply.send(Object.values(users));
+  }
+
+  const result = Object.values(users).filter(
+    (user) => String(user[filter]) === value
+  );
+  return reply.send(result);
+
+  // if (filter && value) {
+  //   const result = Object.values(users).filter(
+  //     (user) => user[filter] == value
+  //   );
+  //   return reply.send(result);
+  // } else {
+  //   return reply.send(Object.values(users));
+  // }
+});
+
 export default fastify;
+
+// fastify.post('/uppercase', (request, reply) => {
+//   return verify('/uppercase', requestBody, reply);
+//   // if (request.body.toLowerCase().includes('fuck')) {
+//   //   return reply.status(403).send('unresolved');
+//   // } else {
+//   //   return reply.send(request.body.toUpperCase());
+//   // }
+// });
+
+// fastify.post('/lowercase', (request, reply) => {
+//   if (request.body.toLowerCase().includes('fuck')) {
+//     return reply.status(403).send('unresolved');
+//   } else {
+//     return reply.send(request.body.toLowerCase());
+//   }
+// });
